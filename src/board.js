1c7de9b2db92371ffa0ce4331016d10e00986585
siobhangi <siobhan.goggins@gmail.com>
2018-08-05 17:35:40 +0100

    Board.js to initialise with a 3x3 2d array

--- src/board.js
+++ src/board.js
@@ -1,5 +1,8 @@
 class Board {
   constructor() {
-    
+    this.template =
+       [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]]
   }
 }
--- test/boardSpec.js
+++ test/boardSpec.js
@@ -1,11 +1,20 @@
-describe('Board', function() {
+describe('Board', () => {
   let board;
 
-  beforeEach(function() {
+  beforeEach(() => {
     board = new Board();
   });
 
-  it('should indicate if board exists', function() {
+  it('should indicate if board exists', () => {
     expect(board).toBeDefined()
+  });
+
+  it('should be a 2d array that contains static information', () => {
+    expect(board.template[0]).toContain(1);
+    expect(board.template[1]).toContain(4);
+    expect(board.template[2]).toContain(7);
+    expect(board.template[0]).not.toContain(4);
+    expect(board.template[1]).not.toContain(1);
+    expect(board.template[2]).not.toContain(1);
   });
 });
