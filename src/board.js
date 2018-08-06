4a5b9207343e3275acd37bcecd0fecebbc85fe83
siobhangi <siobhan.goggins@gmail.com>
2018-08-06 14:16:01 +0100

    BoardFactory function added to create instance of board

--- src/board.js
+++ src/board.js
@@ -11,6 +11,13 @@ class Board {
        [1, 4, 7],
        [2, 5, 8],
        [3, 6, 9],
-       [1, 5, 9]]
+       [1, 5, 9],
+       [3, 5, 7]]
+  };
+}
+
+function BoardFactory() {
+  return {
+    build: () => new Board()
   };
 }
