a89891cca5632eb46f9e51bd0938aee05b16926e
siobhangi <siobhan.goggins@gmail.com>
2018-08-07 14:20:45 +0100

    Remove unnecessary state from player.js constructor

--- src/player.js
+++ src/player.js
@@ -1,13 +1,10 @@
 class Player {
   constructor() {
-    this.MAX_MOVES = 3;
-    this._currentMove = 0;
     this._moves = [];
   }
 
   move(position) {
     this._moves.push(position)
-    this._currentMove++
   }
 
   showMoves() {
--- test/playerSpec.js
+++ test/playerSpec.js
@@ -7,18 +7,7 @@ describe('Player', () => {
     player = playerFactory.build();
   });
 
-  describe('Constructor', () => {
-    it('initalises MAX_MOVE with 3', () => {
-      expect(player.MAX_MOVES).toEqual(3);
-    });
-  });
-
   describe('move', () => {
-    it('increments currentMoves by 1 when called', () => {
-      player.move();
-      expect(player._currentMove).toEqual(1);
-    });
-
     it('pushes position to array', () => {
       player.move(5);
       expect(player._moves).toContain(5);
